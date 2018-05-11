a3c4fb75ed583466600a4a4fccbb3c70a8545b10
SickBandana <39201363+SickBandana@users.noreply.github.com>
2018-05-11 23:37:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const cfg = require('./config.json');
 
 client.on("ready", () => {
     console.log("I am ready");
     client.user.setPresence({ game: { name: 'https://discord.gg/M53KjuE', type: 0 } });
 });
 
-client.login(process.env.BOT_TOKEN);
+//comandi
+client.on('message', async message => {
+    if(message.content ===cfg.prefix + 'ping'){
+       message.reply('pong!'); 
+     }
+})
 
+client.login(process.env.BOT_TOKEN);
