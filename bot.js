8c5323e5b44d87374a71a0fb6e30481a8ccad5fb
SickBandana <39201363+SickBandana@users.noreply.github.com>
2018-05-11 22:30:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const cfg = require('./config.json');
 
-
+client.on("ready", () => {
+    console.log("I am ready!");
+    client.user.setPresence({ game: { name: 'https://discord.gg/M53KjuE', type: 0 } });
+});
 
 client.login(process.env.BOT_TOKEN);
+
