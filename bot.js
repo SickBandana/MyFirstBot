698ac730bfdb2726f21ee36783b4e2579ddf09a9
SickBandana <39201363+SickBandana@users.noreply.github.com>
2018-05-11 22:50:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on("ready", () => {
-    console.log("I am ready!");
+    console.log("I am ready");
     client.user.setPresence({ game: { name: 'Scimmie', type: 0 } });
 });
 
